5eb5e28e54b553904ca4cd3187f9202357709996
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-08-15 07:20:17 -0500

    agregando estilos en css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -8,4 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/home.html'))
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("servidor funcionando en el puerto 3000")
+});
