9a7ca86c7dddf692a432d6d8c49c45ab39044e61
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-08-24 17:08:46 -0500

    modificacion en formularios

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/home.html'))
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post('/login', (req, res) => {
+  console.log("Te logueaste correctamente");
+  res.redirect("/");
+});
+
+app.get('/register', (req, res) => {
+  console.log("Te registraste correctamente");
+  res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post("/register", (req, res) => {
+  res.redirect("/");
+});
+
 app.listen(3000, () => {
   console.log("servidor funcionando en el puerto 3000")
 });
